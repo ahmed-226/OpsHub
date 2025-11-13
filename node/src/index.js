07b6665e3623e09cef60eac378c501d3e35194e0
Mahmoud Hesham <mahmoudhesham656@gmail.com>
2025-11-13 22:50:03 +0200

    updated metrics endpoint to be /metrics

--- node/src/index.js
+++ node/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const urlRoutes = require('./routes/url.routes');
 const db = require('./config/database');
 const metrics = require('./monitoring/metrics');
+const metricsController = require('./controllers/metrics.controller');
+
 
 const app = express();
 const port = 3000;
@@ -10,6 +12,8 @@ app.use(express.json());
 
 
 app.use('/api', urlRoutes);
+
+app.get('/metrics', metricsController.handleMetrics);
 
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
