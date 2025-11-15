ca82051573b65e15a77f0ee08e3ec3d0587498af
Mahmoud Hesham <mahmoudhesham656@gmail.com>
2025-11-15 10:24:14 +0200

    updated the monitoring to work by pods labels

--- configure-prometheus.sh
+++ configure-prometheus.sh
@@ -2,10 +2,12 @@
 set -e
 
 # Define variables for the service, namespace, and port
-SERVICE_NAME="url-shortener-service"
-SERVICE_NAMESPACE="default"
+POD_LABEL="app=url-shortener"
+POD_NAMESPACE="default"
 NAMESPACE="prometheus"
-PORT="3000"
+JOB_NAME="node-app"
+SCRAPE_INTERVAL="5s"
+METRICS_PATH="/metrics"
 
 # Define the ConfigMap name and target
 CONFIGMAP_NAME="prometheus-server"
@@ -15,19 +17,26 @@ minikube kubectl -- get configmap "$CONFIGMAP_NAME" --namespace "$NAMESPACE" -o 
   jq -r '.data["prometheus.yml"]' >prometheus.yml
 
 # Check if the target already exists in the prometheus.yml
-if grep -q "$SERVICE_NAME.$SERVICE_NAMESPACE.svc.cluster.local:$PORT" prometheus.yml; then
-  echo "Target for $SERVICE_NAME already exists in prometheus.yml, no changes made."
+if grep -q "job_name: '$JOB_NAME'" prometheus.yml; then
+  echo "Target for $JOB_NAME already exists in prometheus.yml, no changes made."
 else
   echo "Target not found, adding to prometheus.yml."
-  TARGET_LINE="  - targets: [\"$SERVICE_NAME.$SERVICE_NAMESPACE.svc.cluster.local:$PORT\"]"
 
   # Create a temporary file with the new job config (proper indentation)
   cat >new_job.txt <<EOF
-- job_name: node
-  static_configs:
-$TARGET_LINE
-EOF
+- job_name: '$JOB_NAME'
+  scrape_interval: $SCRAPE_INTERVAL
+  metrics_path: '$METRICS_PATH'
+  kubernetes_sd_configs:
+    - role: pod
+      namespaces:
+        names:
+          - $POD_NAMESPACE
+      selectors:
+        - role: "pod"
+          label: "$POD_LABEL"
 
+EOF
   # Insert the file content after the line containing scrape_configs:
   sed -i '/scrape_configs:/r new_job.txt' prometheus.yml
 
